00c22eaba05c3ac526035d0b22d8142aca89243a
rason <dubstepgunser@mail.ru>
2020-04-13 19:32:34 +0500

    Deleted access token... use argv[1] and argv[2] now

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Bot.bot import Bot
 from Bot.Plugins.CorePlug import CorePlug
 from Bot.Plugins.PluginController import PluginController
+from sys import argv
 def main():
-    bot = Bot(group_id=184995795, token="ea255b16c9a30b7042427bd2e76d72cb12b98304ee6b762d35633c02a6dcdfbfaad89d8316f033230ad0f")
+    bot = Bot(group_id=argv[1], token=argv[2])
     bot.plugins.append(CorePlug(bot))
     bot.plugins.append(PluginController(bot))
     bot.admins.append(205479228)
