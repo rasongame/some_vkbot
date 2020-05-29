9878deb3a19031816cee2b29f847f0969ce15f64
rasongame <dubstepgunser@mail.ru>
2020-05-30 01:12:01 +0500

    Пара говнофич

--- Bot/Plugins/ImagesFromAlbumPlug.py
+++ Bot/Plugins/ImagesFromAlbumPlug.py
@@ -14,21 +14,33 @@ class ImagesFromAlbumPlug:
         self.name = "ImagesFromAlbum"
         self.description = "Send arts from some albums "
         self.version = "rolling"
-        self.keywords = ('каты', "юри", "яой")
+        self.keywords = ('каты', "юри", "яой", 'трапы', 'лоли','ноги', 'ножки')
         self.whoCan = ''
         self.onStart()
 
     def Cats_cmd(self, text, peer_id: int):
         attachment = photo_getWallPhoto(self.bot, "-43228812")
-        self.__sendMessage_with_img(peer_id, "brr?", attachment=attachment)
+        self.__sendMessage_with_img(peer_id, "каты ебать", attachment=attachment)
 
     def Yuri_Cmd(self, text, peer_id: int):
         attachment = photo_getWallPhoto(self.bot, "-63092480")
-        self.__sendMessage_with_img(peer_id, "brr?", attachment=attachment)
+        self.__sendMessage_with_img(peer_id, "Дефки лезбиянки!1!", attachment=attachment)
 
     def Yaoi_Cmd(self, text, peer_id: int):
         attachment = photo_getWallPhoto(self.bot, "-113004231")
-        self.__sendMessage_with_img(peer_id, "brr?", attachment=attachment)
+        self.__sendMessage_with_img(peer_id, "педеростня", attachment=attachment)
+
+    def Loli_Cmd(self, text, peer_id: int):
+        attachment = photo_getWallPhoto(self.bot, "-157516431")
+        self.__sendMessage_with_img(peer_id, "лольки бандерольки", attachment=attachment)
+
+    def Traps_Cmd(self, text, peer_id: int):
+        attachment = photo_getWallPhoto(self.bot, "-171834188")
+        self.__sendMessage_with_img(peer_id, "девочка с подвохом", attachment=attachment)
+
+    def FootFetish_Cmd(self, text, peer_id: int):
+        attachment = photo_getWallPhoto(self.bot, "-102853758")
+        self.__sendMessage_with_img(peer_id, "фетишиста ловите блять", attachment=attachment)
 
     def __sendMessage_with_img(self, peer_id: int, msg: str, attachment: str = "") -> None:
         return self.bot.vk.method("messages.send",
@@ -48,6 +60,13 @@ class ImagesFromAlbumPlug:
             self.Yuri_Cmd(msg, peer_id)
         elif msg.split()[0] == "яой":
             self.Yaoi_Cmd(msg, peer_id)
+        elif msg.split()[0] == "лоли":
+            self.Loli_Cmd(msg, peer_id)
+        elif msg.split()[0] == "трапы":
+            self.Traps_Cmd(msg, peer_id)
+        elif msg.split()[0] == "ноги" or msg.split()[0] == "ножки":
+
+            self.FootFetish_Cmd(msg, peer_id)
         else:
             pass
 
